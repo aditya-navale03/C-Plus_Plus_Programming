52d7499c6f763eb3d2607c50f7bf8d14b13baa31
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2024-12-27 15:34:13 +0530

    example 4 is uploaded

---
+++ Self_learnings_cpp/area_of_circle.cpp
@@ -0,0 +1,18 @@
+//take radius
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << "enter radius:";
+    double radius;
+    cin >> radius;
+
+    const double pi = 3.14;
+
+    double area = pi * pow(radius, 2);
+    cout<<area;
+}
---
+++ Self_learnings_cpp/usingcmath.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+
+    //floor function to remove point after value
+    double result = floor(1.3);
+    cout << result <<endl;
+
+    //pow function 2 to the power of 3
+    double result2 = pow(2, 3);
+    cout << result2;
+
+   
+
+
+
+
+}
