9e0722b2ab3ebfcf78dac12bca1ce1d2160249d5
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2024-12-27 15:49:19 +0530

    initializing variables  is uploaded

---
+++ Self_learnings_cpp/initializing_variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double price = 99.99;
+    float InterestRate = 3.67F;
+    long FileSize = 900000L;
+    char Letter = 'A';
+    bool isValid = false;
+}
