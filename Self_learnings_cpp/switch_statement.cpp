0f8644453bca5cd6400a401d77be0a6f7b07e09c
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2024-12-30 11:36:26 +0530

    switch case is added

--- Self_learnings_cpp/switch_statement.cpp
+++ Self_learnings_cpp/switch_statement.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 using namespace std;
-int main() {
-  
-  return 0;
+int main()
+{
+
+    // switch operation
+    // calculater
+
+    cout << "1.addition(+)" << endl
+         << "2.subtraction(-)" << endl
+         << "3.multliplication(*)" << endl
+         << "4.dividation(/)" << endl;
+     char operation;
+    cout << "enter what operation to do:";
+    cin>>operation;
+   
+
+    cout << "enter two numbers:";
+    int number1, number2;
+    cin >> number1 >> number2;
+
+    switch (operation)
+    {
+    case '+':
+        cout << "addition is:" << number1 + number2;
+        break;
+
+    case '-':
+        cout << "subtraction is :" << number1 - number2;
+        break;
+
+    case '*':
+        cout << "multliplication is:" << number1 * number2;
+        break;
+
+    case '/':
+        cout << "dividation is:" << number1 / number2;
+        break;
+
+    default:
+        "enter valid input";
+    }
+
+    return 0;
 }
